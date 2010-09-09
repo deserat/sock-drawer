ccab307b5da9a537d589947c60efdad2d51f8dd3
Vance Dubberly <vance@themojave.com>
2010-09-09 00:56:25 -0700

    insert faye into the systems in place of Socket.IO

--- server.js
+++ server.js
@@ -2,7 +2,17 @@ require.paths.unshift(__dirname + "/vendor");
 
 var http = require('http'),
     sys  = require('sys'),
-    static = require('node-static/lib/node-static');
+    static = require('node-static/lib/node-static'),
+    faye = require('faye-node');
+
+
+
+process.addListener('uncaughtException', function (err, stack) {
+  console.log('------------------------');
+  console.log('Exception: ' + err);
+  console.log(err.stack);
+  console.log('------------------------');
+});
 
 
 var server = http.createServer(function(request, response) {
