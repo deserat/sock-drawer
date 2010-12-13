0152ea0cef7712fc00ddd62ce80feab218464dd1
Vance Dubberly <vance@themojave.com>
2010-12-13 02:09:37 -0800

    ok now there is a very basic drawing tool. communication still non-existant

--- public/js/script.js
+++ public/js/script.js
@@ -11,9 +11,9 @@ var socket;
 var events = [];
 var client;
 var myId;
+var brush;
 
 MOUNT_POINT = 'http://192.168.1.102:8000/drawer';
-
 
 function drawPosition(x,y) { return { x: x - coords.l, y: y - coords.t }; }
 
@@ -27,9 +27,9 @@ dojo.addOnLoad( function(e){
     myId = Math.floor(Math.random()*11)
     console.log(myId);
 
-    //client = new Faye.Client(MOUNT_POINT, {timeout:240});
+    brush = new Brush({});
 
-    console.log(client);
+    //client = new Faye.Client(MOUNT_POINT, {timeout:240});
 
     //var subscription = client.subscribe('/draw', function(m) {
     //    if ( m.user != myId ) {
@@ -39,49 +39,73 @@ dojo.addOnLoad( function(e){
     //    }
     //});
 
-dojo.connect(canvas,'mouseover', function (e) {
-    console.log("enter canvas");
-    console.log(e);
-});
-
-dojo.connect(canvas, 'mousedown', function (e) {
-
-    var p =  drawPosition(e.pageX, e.pageY);
-    
-    ctx.beginPath(); 
-    ctx.moveTo(p.x, p.y);
-
-    events['paint'] = dojo.connect(canvas, 'mousemove', function (e) {
-       p = drawPosition(e.pageX, e.pageY);
-       ctx.lineTo(p.x,p.y);
-       ctx.stroke(); 
-       //client.publish('/draw', {user:myId,x:p.x,y:p.y});
-       //dojox.cometd.publish('/draw', {user:myId,x:p.x,y:p.y});
+    dojo.connect(canvas,'mouseover', function (e) {
+        console.log("enter canvas");
+        console.log(e);
     });
-});
+
+    dojo.connect(canvas, 'mousedown', function (e) {
+        console.log("mousedown");
+        var p =  drawPosition(e.pageX, e.pageY);
+        ctx.strokeStyle = brush.color.toString();
+        ctx.lineWidth = brush.size;
+        ctx.lineCap = brush.lineCap;
+        ctx.lineJoin = brush.lineJoin;
+        ctx.beginPath(); 
+        ctx.moveTo(p.x,p.y);
+
+        events['paint'] = dojo.connect(canvas, 'mousemove', function (e) {
+            p = drawPosition(e.pageX, e.pageY);
+            brush.stroke(p, ctx );
+            //ctx.lineTo(p.x,p.y);
+            //ctx.stroke(); 
+            //client.publish('/draw', {user:myId,x:p.x,y:p.y});
+            //dojox.cometd.publish('/draw', {user:myId,x:p.x,y:p.y});
+        });
+    });
 
 
-dojo.connect(canvas,'mouseup', function (e) {
-    dojo.disconnect(events['paint']);
-});
+    dojo.connect(canvas,'mouseup', function (e) {
+        dojo.disconnect(events['paint']);
+    });
 
-// when mouse leaves canvas unbind the move event
-dojo.connect(canvas,'mouseout', function (e) {
-    dojo.disconnect(events['paint']);
+    // when mouse leaves canvas unbind the move event
+    dojo.connect(canvas,'mouseout', function (e) {
+        dojo.disconnect(events['paint']);
+    });
 
-});
-
-
+    dojo.query('.brush_setter').connect('onchange', function(e) {
+        var el = e.currentTarget;
+        var id = el.id;
+        console.log(el);
+        console.log(id);
+        if ( id == 'size' || id == 'lineJoin' || id == 'lineCap' ) {  
+            console.log(el.options[el.options.selectedIndex].value);
+            brush[id] = el.options[el.options.selectedIndex].value;
+        } else if ( id == 'color' ) {
+            c =  el.options[el.options.selectedIndex].value.split(',');
+            brush.color.r = c[0];  
+            brush.color.g = c[1]  
+            brush.color.b = c[2]  
+            console.log(brush.color);
+        } else if ( id == 'opacity' ) {
+            brush.color.a = el.options[el.options.selectedIndex].value;  
+        } else if ( id == 'stroke' ) {
+            brush.stroke = getStroke(el.options[el.options.selectedIndex].value);  
+        }
+    });
 });
 
 dojo.declare("Color", null, {
     constructor : function(r,g,b,a) {
-        this.r = r;
-        this.g = g;
-        this.b = b;
-        this.a = a;
+        this.r = ( r != undefined ) ? r : 0;
+        this.g = ( g != undefined ) ? g : 0;
+        this.b = ( b != undefined ) ? b : 0;
+        this.a = ( a != undefined ) ? a : 1;
     },
     toString : function() {
+        console.log("toString");
+        console.log( 'rgba(' + this.r + ',' + this.g + ',' + this.b + ',' + this.a + ')');
         return 'rgba(' + this.r + ',' + this.g + ',' + this.b + ',' + this.a + ')';
     },
 });
@@ -91,10 +115,34 @@ dojo.declare("Color", null, {
 
 // simple line
 
-function lineStroke(points,ctx) {
-    ctx.lineTo(p.x,p.y);
-    ctx.stroke();
+function getStroke(stroke) {
+
+if (stroke == 'line') {
+    return function lineStroke(points,ctx) {
+        ctx.lineTo(points.x,points.y);
+        ctx.stroke();
+    }
+} else if ( stroke == 'spines' ) { 
+    return function spineStroke(points,ctx) {
+        ctx.beginPath(); 
+        ctx.moveTo(points.x-10,points.y-10);
+        ctx.lineTo(points.x+10,points.y+10);
+        ctx.stroke();
+    }
+} else if ( stroke == 'dots' ) { 
+    return function dotStroke(points,ctx) {
+        ctx.beginPath(); 
+        ctx.moveTo(points.x,points.y);
+        ctx.lineTo(points.x,points.y);
+        ctx.stroke();
+
+    }
+
 }
+}
+
+
+
 
 /** 
  * Object used in Paper for determining stroke on canvas.
@@ -104,12 +152,16 @@ function lineStroke(points,ctx) {
 dojo.declare("Brush", null, {
     size: null,
     color: null,
-    // a function that controls how the brush strokes if not defined just solid line 
-    stroke: 'solid',
+    stroke: null,
+    lineCap: null,
+    lineJoin: null,
+
     constructor : function(args) {
         this.size = (args.size != undefined) ? args.size : 5;
-        this.color = (args.color != undefined) ? args.color : 'rgba(0,0,0,1)';
-        this.stroke = (args.stroke != undefined) ? args.stroke : lineStroke;
+        this.color = new Color();
+        this.stroke = (args.stroke != undefined) ? args.stroke : getStroke('line');
+        this.lineJoin = (args.lineJoin != undefined) ? args.lineJoin : 'round';
+        this.lineCap = (args.lineCap != undefined) ? args.lineCap : 'round';
     }
 });
 
